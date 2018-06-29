fbac9cb772e3e657ef599ffaab2e87e285de5443
Dan Elias <daniel@elias-family.com>
2018-06-29 15:51:33 +1000

    fixes to requirements.txt and associated files for Travis-CI and ReadTheDocs

--- {{cookiecutter.app_name}}/initialize.sh
+++ {{cookiecutter.app_name}}/initialize.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+this_script=`basename "$0"`
+requirements_build=requirements_build.txt
+requirements_doc=requirements_doc.txt
+
 function show_usage {
   echo "USAGE: $0 [options]"
   echo "Options:"
@@ -25,21 +29,18 @@ do
   esac
 done
 
-this_script=`basename "$0"`
-
 for script_name in *.sh
 do
   chmod +x $script_name
 done
-
-build_requirements=build_requirements.txt
 
 git init
 git add .
 git reset $this_script
 git commit -m 'Initialization'
 git remote add origin git@github.com:{{ cookiecutter.github_username }}/{{ cookiecutter.app_name }}.git
-pip install -r $build_requirements
+pip install -r $requirements_build
+pip install -r $requirements_doc
 ./run_tests.sh
 ./update_docs.sh
 
@@ -49,8 +50,11 @@ then
   git push -u origin master
   pip install -e .
   rm $this_script
-  rm $build_requirements
   echo "Manual steps:"
   echo "  * Go to https://readthedocs.org/dashboard/ and import {{ cookiecutter.app_name }}"
   echo "  * Go to https://travis-ci.org/profile/{{ cookiecutter.github_username }}, Sync the account and enable tests for {{ cookiecutter.app_name }}"
+  echo "  * Make these changes on https://readthedocs.org/dashboard/{{ cookiecutter.app_name }}/advanced/ :"
+  echo "    - check 'Install Project'"
+  echo "    - in the 'Requirements file' box, type: requirements_rtd.txt"
+  echo "    - set the Python interpreter to CPython3.x"
 fi
